409341224d73e2599163ddae020b30b804477442
Atif Ali <atifsyedali@gmail.com>
2019-11-06 20:37:26 -0500

    Extract issue numbers from attached commits in PR

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,50 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as octokit from '@octokit/rest'
 
 type PRInfo = {
   branch: string
   prNumber: string
+  commits: octokit.PullsListCommitsResponse
 }
 
 async function run() {
   try {
     const token = core.getInput('GITHUB_TOKEN', { required: true })
 
-    const prInfo = getPRInfo()
+    const prInfo = await getPRInfo()
     if (!prInfo) {
       console.log('Could not get the branch name from context, exiting')
       return
     }
 
-    const { branch, prNumber } = prInfo
-    let issueNumber
-    for (const part of branch.split(/[-:_\W#]+/g)) {
-      issueNumber = parseInt(part)
-      if (issueNumber) {
-        break
+    // first we look at the branch name...
+    const { branch, prNumber, commits } = prInfo
+    const issueNumbers: number[] = []
+    const branchIssueNumber = extractIssueNumber(branch)
+    if (branchIssueNumber) {
+      issueNumbers.push(branchIssueNumber)
+    }
+
+    // then we look at the commits...
+    if (commits) {
+      for (const commitRelation of commits) {
+        const message = commitRelation.commit.message
+        const commitIssueNumber = extractIssueNumber(message)
+        if (commitIssueNumber) {
+          issueNumbers.push(commitIssueNumber)
+        }
       }
     }
 
-    if (!issueNumber) {
+    if (issueNumbers.length === 0) {
       console.log(
-        `Could not get the issueNumber from branch name ${branch}, exiting`
+        `Could not get the issueNumber from branch name ${branch} or related commits, exiting`
       )
       return
     }
 
-    const text = `This PR closes: #${issueNumber}`
+    const text = `This PR closes #${issueNumbers.join(', #')}`
 
     const client = new github.GitHub(token)
     await createComment(client, prNumber, text)
@@ -42,15 +54,37 @@ async function run() {
   }
 }
 
-function getPRInfo(): PRInfo | undefined {
-  const pr = github.context.payload.pull_request
+function extractIssueNumber(str: string): number | undefined {
+  for (const part of str.split(/[-:_\W#\/]+/g)) {
+    const maybeIssueNumber = parseInt(part)
+    if (maybeIssueNumber) {
+      return maybeIssueNumber
+    }
+  }
+}
+
+async function getPRInfo(): Promise<PRInfo | undefined> {
+  const payload = github.context.payload
+  console.log(JSON.stringify(github.context.payload, undefined, 2))
+
+  const pr = payload.pull_request
   if (!pr) {
     return
   }
 
+  const token = core.getInput('GITHUB_TOKEN', { required: true })
+  const client = new github.GitHub(token)
+  const commits = await client.pulls.listCommits({
+    pull_number: pr.number,
+    owner: pr.base.repo.owner.login,
+    repo: pr.base.repo.name,
+    per_page: 100,
+  })
+
   return {
     branch: pr.head.ref,
     prNumber: pr.node_id,
+    commits: commits.data,
   }
 }
 
